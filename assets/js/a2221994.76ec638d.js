66c91ba9e7c6e84c6c5c6bc622f069120948b6fb
Andrii Bondariev <andreybond13@gmail.com>
2024-05-24 18:10:40 +0000

    Deploy to GitHub Pages

--- assets/js/a2221994.76ec638d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktech_notes=self.webpackChunktech_notes||[]).push([[3648],{8262:i=>{i.exports=JSON.parse('{"title":"Scripting","description":"Scripting skills","slug":"/category/scripting","permalink":"/category/scripting","navigation":{"previous":{"title":"logging","permalink":"/logging/"},"next":{"title":"Python","permalink":"/scripting/python"}}}')}}]);
